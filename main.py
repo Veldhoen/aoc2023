7ba234f26d410c041ea08cef92fafe03631fc7f8
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-12-10 13:21:14 +0100

    main.py with aoc_solver class, WIP

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import aoc_5
+import aocd
+import os
 
-aoc_5.solve()
+
+class AOC_solver():
+    def __init__(self, day_number):
+        self.datapath = os.path.join('data',f"{day_number}.input")
+        if not os.path.exists(self.datapath):
+            self.data = aocd.get_data(day=day)
+            with open(self.datapath, 'w') as f:
+                f.write(self.data)
+
+
